9753364a7efd42ec5181c43a22422f9d79d9a390
kazuyuki-nakamura <aptx1400kawasaki@gmail.com>
2020-09-10 15:57:49 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :dvise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname,:email,:password,:encrypted_password,:first_name,:last_name,:first_name_kana,:last_name_kana,:birth_day ])
+  end
 end
