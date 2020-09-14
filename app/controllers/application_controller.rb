4b20d1aa72134ded9127cd0e5757b49a50f60bcd
kazuyuki-nakamura <aptx1400kawasaki@gmail.com>
2020-09-14 15:39:44 +0900

    devise_parameter_sanitizer.permitの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :dvise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname,:email,:password,:encrypted_password,:first_name,:last_name,:first_name_kana,:last_name_kana,:birth_day ])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname,:email,:password,:first_name,:last_name,:first_name_kana,:last_name_kana,:birth_day ])
   end
 end
