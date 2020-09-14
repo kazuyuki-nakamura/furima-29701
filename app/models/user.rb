e763354e579cec6f45a479e66e5a9f5817b83a79
kazuyuki-nakamura <aptx1400kawasaki@gmail.com>
2020-09-14 14:57:13 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ validates :nickname, :email,:password,:first_name,:last_name,:first_name_kana,:last_name_kana,:birth_day, presence: true    
+
+         
 end
