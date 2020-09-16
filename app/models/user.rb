0dcb892b765f7e78d9c2ec0d972bff366503022a
kazuyuki-nakamura <aptx1400kawasaki@gmail.com>
2020-09-16 14:15:03 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
- validates :nickname, :email,:password,:first_name,:last_name,:first_name_kana,:last_name_kana,:birth_day, presence: true    
+ validates :nickname, :email,:password,:first_name,:last_name,:first_name_kana,:last_name_kana,:birth_day, presence: true 
+ validates :password, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i} 
+ validates :password, length: { minimum: 6 }
+ validates :email, format { with: VALID_EMAIL_REGEX }
+ validates :email, format { /^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/ }
+ validates :first_name, format { /\A[ぁ-んァ-ン一-龥]/+\z }
+ validates :last_name, format { /\A[ぁ-んァ-ン一-龥]/+\z }
+ validates :first_name_kana, format { /\p{katakana}//\A[ァ-ヶー－]+\z/ }
+ validates :last_name_kana, format { /\p{katakana}//\A[ァ-ヶー－]+\z/ }
+
+
+
+
 
          
 end
