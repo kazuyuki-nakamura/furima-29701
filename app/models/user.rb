b8bbf853c0e313eb782ab237b31d56601dbf31c8
kazuyuki-nakamura <aptx1400kawasaki@gmail.com>
2020-09-17 12:14:26 +0900

    birth_dayのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]/+\z } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
     validates :first_name
     validates :last_name 
+    validates :nickname
   end
 
-  with_options presence: true, format: { with:/\p{katakana}//\A[ァ-ヶー－]+\z/ } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
     validates :first_name_kana
     validates :last_name_kana
   end
@@ -17,8 +18,10 @@ class User < ApplicationRecord
     PASSWORD_REGEX = /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i 
     validates_format_of :password, with: PASSWORD_REGEX
     validates :password, length: { minimum: 6 }
-    VALID_EMAIL_REGEX = /^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/
-    validates_format_of :emal, with: VALID_EMAIL_REGEX
+    VALID_EMAIL_REGEX = /\A([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+\z/
+    validates_format_of :email, with: VALID_EMAIL_REGEX
+    
+    validates :birth_day, presence: true
     
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:  "items#index"
-  resources :users, only: [:index, :edit, :update, :new, :create]
+  resources :users, only: [:index, :new, :create ]
 end
 
